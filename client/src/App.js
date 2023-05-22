a1eb5fe81b292c51ed3887f929fad61a12ffa9e1
sarasg89 <118869836+sarasg89@users.noreply.github.com>
2023-05-21 22:13:59 -0400

    updated to latest versions of npm packages of react, react router and react dom so that app renders properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -36,7 +36,7 @@ function App() {
       <Router>
         <>
           <Navbar />
-          <Switch>
+          <Routes>
             <Route
               path='/'
               element={<SearchBooks />}
@@ -52,7 +52,7 @@ function App() {
                   className='display-2'>Wrong page!
                 </h1>}
             />
-          </Switch>
+          </Routes>
         </>
       </Router>
     </ApolloProvider>
